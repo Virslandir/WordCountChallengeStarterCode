09977ecf0dda31209772f68aca4e25ac31671564
Mateusz Chrupczalski <m.chrupczalski@outlook.com>
2023-04-10 03:06:46 +0100

    Add Runner to use the DataService

---
+++ ConsoleUI/Runner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CoreLib;
+
+namespace ConsoleUI
+{
+    public class Runner
+    {
+        private readonly IDataService _dataService;
+
+        public Runner(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(_dataService.DisplayTotalWordCount("Hello World"));
+            consoleViewer.DisplayTotalWordCount(text1);
+            consoleViewer.DisplayTotalCharacterCount(text1);
+            consoleViewer.DisplayTotalCharacterCountMinusLineReturnsAndWhitespaces(text1);
+            consoleViewer.DisplayMostUsedWord(text1);
+            consoleViewer.DisplayMostUsedCharacter(text1);
+        }
+    }
+}
